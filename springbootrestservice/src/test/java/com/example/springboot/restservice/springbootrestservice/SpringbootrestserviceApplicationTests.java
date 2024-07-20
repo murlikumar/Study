bdfc18fe39a2cbf13eb780e7b70eae268eb1e21a
murlikumar <murlikumar85@gmail.com>
2024-07-20 11:40:41 +0530

    tset

---
+++ springbootrestservice/src/test/java/com/example/springboot/restservice/springbootrestservice/SpringbootrestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.restservice.springbootrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
